b71a00dd8f098e4da3e0060f1c6cc06b110417f7
pandikapinata <pandikapinata27@gmail.com>
2018-07-03 19:15:37 +0800

    Encoding User Input

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 // import request from 'request';
 const request = require('request');
 const key = require('./key.js');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a:{
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        },
+    })
+    .help()
+    .alias('help','h')
+    .argv;
+
+var encodedAddress = encodeURIComponent(argv.a);
 
 request({
     // url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia%20key=AIzaSyBo8ZKUY9-WRn_FptuLS-hnm09rqSvoJFI',
-    url: `https://maps.googleapis.com/maps/api/geocode/json?&address=Denpasar%20key=${key.getKey()}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?&address=${encodedAddress}%20key=${key.getKey()}`,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
