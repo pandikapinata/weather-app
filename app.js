efe2a9be1597984d86fd135291bba10a0b3c57fa
pandikapinata <pandikapinata27@gmail.com>
2018-07-03 18:58:16 +0800

    Get Object in JSON HTTP Request

--- app.js
+++ app.js
@@ -7,5 +7,7 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?&address=Denpasar%20key=${key.getKey()}`,
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
